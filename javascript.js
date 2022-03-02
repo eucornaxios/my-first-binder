7915c4be82d0319e6348709fe107192584cbe886
eucornaxios <97867792+eucornaxios@users.noreply.github.com>
2022-03-02 00:54:13 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
-function compute(){
+function calculate()
+{
   
+   var Capital = document.getElementById("C").value;
+   var Years = document.getElementById("y").value;
+   var Rate = document.getElementById("R").value;
+   var result = document.getElementById("result");
+    
+    result.innerHTML = "Your  interest most likely be" + (C*Y*R/100);
+    
+    
+    
 }
