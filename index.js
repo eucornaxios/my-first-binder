f633c92956f5a9963e413845f618f4f1bef99dcc
eucornaxios <97867792+eucornaxios@users.noreply.github.com>
2022-03-07 00:42:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
-var http = require("http");
+const http = require('http');
 
- http.createServer(function (teq, res) {
-      res.writeHead(200, {'Content-Type':
-    'text/html'});
-        res.end("Hello Wirld!");
-}).listen(8080);
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+}
+
+const port = 8080;
+const server = http.createServer(requestListener);
+console.log('server listening on port: ' + port);
